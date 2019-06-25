9b5cac68110f486c7bd64dfaee3a41a60f66e283
msholomukund <45308771+msholomukund@users.noreply.github.com>
2019-06-25 20:30:25 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': classes[int(prediction[1])]})
+    return JSONResponse({'result': classes[1]})
 
 
 if __name__ == '__main__':
